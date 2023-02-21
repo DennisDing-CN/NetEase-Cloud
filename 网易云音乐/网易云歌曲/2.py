2e082c6b9a0861133ed7450c5de0fb1e3f9b5288
DennisDing_CN <105276701+DennisDing-CN@users.noreply.github.com>
2023-02-21 17:42:05 +0800

    Add files via upload

---
+++ 网易云音乐/网易云歌曲/2.py
@@ -0,0 +1,23 @@
+import requests, execjs
+
+with open('2.js', 'r', encoding='utf8') as f:
+    line = f.read()
+
+par = execjs.compile(line).call('jiami', 1895164923)
+# print(par)
+headers={
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+}
+data={
+    'params': par['encText'],
+    'encSecKey': par['encSecKey'],
+}
+# print(data)
+# proxies = {
+# 			"https": "http://127.0.0.1:8888",
+#      	 }
+response = requests.post('https://music.163.com/weapi/song/enhance/player/url/v1?csrf_token=', headers=headers, data=data)
+print(response.text)
+# url = response['data'][0]['url']
+# with open('3.mp3', 'wb') as f:
+#     f.write(requests.get(url).content)
---
+++ 网易云音乐/网易云评论/main.py
@@ -0,0 +1,20 @@
+import requests, execjs
+
+with open('lyrics.js', 'r', encoding='utf-8')  as f:
+    line = f.read()
+
+music_id = 1970421414
+huoqu = execjs.compile(line).call('get_form', music_id)
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+}
+data = {
+    'params': huoqu['encText'],
+    'encSecKey': huoqu['encSecKey']
+}
+# print(data)
+url = 'https://music.163.com/weapi/comment/resource/comments/get?csrf_token='
+response = requests.post(url, headers=headers, data=data)
+print(response)
+print(response.text)
